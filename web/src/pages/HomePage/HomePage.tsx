0e7bb1f6b24db080e721fa21941d1257dc67e6f0
Thomas Esimple <thomas.cipriani@esimple.it>
2022-06-08 14:19:21 +0200

    Updated Home page content

--- web/src/pages/HomePage/HomePage.tsx
+++ web/src/pages/HomePage/HomePage.tsx
@@ -1,4 +1,3 @@
-import { Link, routes } from '@redwoodjs/router'
 import { MetaTags } from '@redwoodjs/web'
 
 const HomePage = () => {
@@ -7,13 +6,7 @@ const HomePage = () => {
       <MetaTags title="Home" description="Home page" />
 
       <h1>HomePage</h1>
-      <p>
-        Find me in <code>./web/src/pages/HomePage/HomePage.tsx</code>
-      </p>
-      <p>
-        My default route is named <code>home</code>, link to me with `
-        <Link to={routes.home()}>Home</Link>`
-      </p>
+      <p>Welcome to the blog</p>
     </>
   )
 }
