291e3a71f84110623e9410e049173d23547b29c1
Thomas Esimple <thomas.cipriani@esimple.it>
2022-06-08 16:23:24 +0200

    Contact form

--- web/src/pages/ContactPage/ContactPage.tsx
+++ web/src/pages/ContactPage/ContactPage.tsx
@@ -1,10 +1,36 @@
-import { Link, routes } from '@redwoodjs/router'
+import {
+  Form,
+  Submit,
+  SubmitHandler,
+  TextAreaField,
+  TextField,
+} from '@redwoodjs/forms'
 import { MetaTags } from '@redwoodjs/web'
 
+interface FormValues {
+  name: string
+  email: string
+  message: string
+}
+
 const ContactPage = () => {
+  const onSubmit: SubmitHandler<FormValues> = (data) => {
+    console.log(data)
+  }
   return (
     <>
       <MetaTags title="Contact" description="Contact page" />
+
+      <Form onSubmit={onSubmit}>
+        <label>Name</label>
+        <TextField name="name" />
+        <label>EMail</label>
+        <TextField name="email" />
+        <label>Message</label>
+        <TextAreaField name="message" />
+
+        <Submit>Send Messagge</Submit>
+      </Form>
     </>
   )
 }
