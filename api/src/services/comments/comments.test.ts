2c84a18119630d70f739db55cc78fd1aa33925ea
Thomas Esimple <thomas.cipriani@esimple.it>
2022-06-14 16:25:52 +0200

    Added createComment tests

--- api/src/services/comments/comments.test.ts
+++ api/src/services/comments/comments.test.ts
@@ -1,5 +1,5 @@
-import { comments } from './comments'
-import type { StandardScenario } from './comments.scenarios'
+import { comments, createComment } from './comments'
+import type { StandardScenario, PostOnlyScenario } from './comments.scenarios'
 
 // Generated boilerplate tests do not account for all circumstances
 // and can fail without adjustments, e.g. Float and DateTime types.
@@ -13,4 +13,24 @@ describe('comments', () => {
 
     expect(result.length).toEqual(Object.keys(scenario.comment).length)
   })
+
+  scenario(
+    'postOnly',
+    'creates a new comments',
+    async (scenario: PostOnlyScenario) => {
+      const commentData = {
+        name: 'Billy Bob',
+        body: 'What is your favorite tree bark?',
+        postId: scenario.post.bark.id,
+      }
+      const comment = await createComment({
+        input: commentData,
+      })
+
+      expect(comment.name).toEqual(commentData.name)
+      expect(comment.body).toEqual(commentData.body)
+      expect(comment.postId).toEqual(scenario.post.bark.id)
+      expect(comment.createdAt).not.toEqual(null)
+    }
+  )
 })
