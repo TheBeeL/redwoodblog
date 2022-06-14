5bd4d8a82034c713f627094dd4ac85515d45ea96
Thomas Esimple <thomas.cipriani@esimple.it>
2022-06-14 16:25:22 +0200

    Update comments service testing scenario

--- api/src/services/comments/comments.scenarios.ts
+++ api/src/services/comments/comments.scenarios.ts
@@ -1,22 +1,41 @@
-import type { Prisma } from '@prisma/client'
+import { Prisma } from '@prisma/client'
 
 export const standard = defineScenario<Prisma.CommentCreateArgs>({
   comment: {
-    one: {
+    jane: {
       data: {
-        name: 'String',
-        body: 'String',
-        post: { create: { title: 'String', body: 'String' } },
+        name: 'Jane Doe',
+        body: 'I like trees',
+        post: {
+          create: {
+            title: 'Redwood Leaves',
+            body: 'The quick brown foc jumped over the lazy dog.',
+          },
+        },
       },
     },
-    two: {
+    john: {
       data: {
-        name: 'String',
-        body: 'String',
-        post: { create: { title: 'String', body: 'String' } },
+        name: 'John Doe',
+        body: 'Hug a tree today',
+        post: {
+          create: {
+            title: 'Root Systems',
+            body: 'The five boxing wizards jump quickly.',
+          },
+        },
       },
     },
   },
 })
 
+export const postOnly = defineScenario<Prisma.PostCreateArgs>({
+  post: {
+    bark: {
+      data: { title: 'Bark', body: "A tree's bark is worse than its bite" },
+    },
+  },
+})
+
 export type StandardScenario = typeof standard
+export type PostOnlyScenario = typeof postOnly
