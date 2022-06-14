630230b261d2ffd461f1981d442a69091c8ea28c
Thomas Esimple <thomas.cipriani@esimple.it>
2022-06-14 17:48:23 +0200

    Mocked CreateCommentMutation

--- web/src/components/CommentForm/CommentForm.stories.tsx
+++ web/src/components/CommentForm/CommentForm.stories.tsx
@@ -1,6 +1,20 @@
 import CommentForm from './CommentForm'
 
 export const generated = () => {
+  mockGraphQLMutation('CreateCommentMutation', (variables, { ctx }) => {
+    const id = Math.floor(Math.random() * 1000)
+    ctx.delay(1000)
+
+    return {
+      createComment: {
+        id,
+        name: variables.input.name,
+        body: variables.input.body,
+        cretedAt: new Date().toISOString(),
+      },
+    }
+  })
+
   return <CommentForm />
 }
 
