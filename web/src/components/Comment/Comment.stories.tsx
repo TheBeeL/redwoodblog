8d53482ffc540f78f312619e335f9ec73a03c438
Thomas Esimple <thomas.cipriani@esimple.it>
2022-06-14 14:46:18 +0200

    Updated Comment component stories

--- web/src/components/Comment/Comment.stories.tsx
+++ web/src/components/Comment/Comment.stories.tsx
@@ -1,7 +1,15 @@
 import Comment from './Comment'
 
 export const generated = () => {
-  return <Comment />
+  return (
+    <Comment
+      comment={{
+        name: 'Tom Cameron',
+        body: 'This is the first comment',
+        createdAt: '2022-01-01T12:34:56Z',
+      }}
+    />
+  )
 }
 
 export default { title: 'Components/Comment' }
