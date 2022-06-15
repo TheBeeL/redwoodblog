f09aa9d87729ee06d3beb455e094ff86bb21c8d0
Thomas Esimple <thomas.cipriani@esimple.it>
2022-06-15 14:14:37 +0200

    Added moderator view in comment stories

--- web/src/components/Comment/Comment.stories.tsx
+++ web/src/components/Comment/Comment.stories.tsx
@@ -1,14 +1,32 @@
 import Comment from './Comment'
 
-export const generated = () => {
+const COMMENT = {
+  id: 1,
+  name: 'Tom Cameron',
+  body: 'This is the first comment',
+  createdAt: '2022-01-01T12:34:56Z',
+  postId: 1,
+}
+
+export const defaultView = () => {
   return (
-    <Comment
-      comment={{
-        name: 'Tom Cameron',
-        body: 'This is the first comment',
-        createdAt: '2022-01-01T12:34:56Z',
-      }}
-    />
+    <div className="m-4">
+      <Comment comment={COMMENT} />
+    </div>
+  )
+}
+
+export const moderatorView = () => {
+  mockCurrentUser({
+    id: 1,
+    roles: 'moderator',
+    email: 'moderator@moderator.com',
+  })
+
+  return (
+    <div className="m-4">
+      <Comment comment={COMMENT} />
+    </div>
   )
 }
 
